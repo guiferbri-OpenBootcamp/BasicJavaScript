06471ea1c7014be202f5e6f9aed889311e202463
Guiomar <guio.fbb@gmail.com>
2022-07-18 19:03:35 +0200

    Tema 2 - ejercicio 1

---
+++ Tema2/T1Ej2.js
@@ -0,0 +1,12 @@
+var elements = [
+    "Guiomar",
+    28,
+    true,
+    new Date("1993-08-06"),
+    {
+        title : "La catedral del mar",
+        author : "Ildefonso Falcones",
+        date : "07/01/2011",
+        url : "https://www.casadellibro.com/libro-la-catedral-del-mar/9788499088044/1819720"
+    }
+]
