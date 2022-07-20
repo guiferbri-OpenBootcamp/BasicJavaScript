2a0673c989cf59884fe362e7e7c2b0782528aaf2
Guiomar <guio.fbb@gmail.com>
2022-07-20 09:03:41 +0200

    Tema 5 - ejercicio 1

---
+++ Tema5/T5Ej1.js
@@ -0,0 +1,6 @@
+var highCm = 174;
+var highM = highCm/100;
+var weight = 62.2;
+var highRound = Math.ceil(highM);
+var weightRound = Math.floor(weight);
+var maxNumber = Number.MAX_VALUE + 1 == Number.MAX_VALUE;
