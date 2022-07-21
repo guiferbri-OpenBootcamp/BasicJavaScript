8bf11134026736b2a47e0751a45cfa449f37b125
Guiomar <guio.fbb@gmail.com>
2022-07-21 19:44:15 +0200

    Tema 7 - ejercicio 3

---
+++ Tema7/fechas.js
@@ -0,0 +1,6 @@
+let today = new Date();
+let birthday = new Date(1993,07,06);
+let compare = today > birthday;
+let birthdayDay = birthday.getDate();
+let birthdayMonth = birthday.getMonth() + 1;
+let birthdayYear = birthday.getFullYear();
