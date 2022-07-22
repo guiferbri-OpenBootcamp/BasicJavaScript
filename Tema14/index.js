df568c2f5e6bcd8c974bfd3a0adcb3eada63b4f5
Guiomar <guio.fbb@gmail.com>
2022-07-22 17:42:50 +0200

    Tema 14 - ejercicio 1

---
+++ Tema14/index.js
@@ -0,0 +1,4 @@
+let button = document.getElementById("button");
+button.addEventListener("click", () => alert("click en el botón"));
+
+$('#button').click(() => console.log("Hola, estoy utilizando jQuery"));
